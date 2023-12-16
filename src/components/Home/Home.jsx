c6973a185719a99045e1cc6401f512f16aba66f8
Piyawat Wongyat <mask30254@gmail.com>
2023-12-16 16:21:48 +0700

    Moved from POSYAYEE-V2 to develop a separate web page because deployment is difficult to manage data and may be very prone to errors.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import Navbar from "../Navbar/Navbar";
+import SystemAbility from "./SystemAbility";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <SystemAbility/>
+    </>
+  );
+}
+
+export default Home;
